396f6dedd4d647d8b830ad7b7123b6c26ee09b39
João Pedro <127902932+jotapelopes@users.noreply.github.com>
2023-11-22 21:59:44 -0300

    Create Inventario.php

---
+++ Inventario.php
@@ -0,0 +1,18 @@
+<?php
+
+class Inventario
+{
+    private int $capacidadeMaxima;
+    private $itens;
+
+    public function __construct($capacidadeMaxima, $itens)
+    {
+        $this->capacidadeMaxima = $capacidadeMaxima;
+        $this->itens = array[]; 
+    }
+
+    public function getCapacidadeMaxima()
+    {
+        
+    }
+}
