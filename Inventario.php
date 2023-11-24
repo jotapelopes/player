af6df8cc555fcd91ebc1f8c4960d8b057c7dc27f
João Pedro <127902932+jotapelopes@users.noreply.github.com>
2023-11-23 21:20:06 -0300

    Update Inventario.php - v.final

--- Inventario.php
+++ Inventario.php
@@ -5,7 +5,7 @@ class Inventario
     private int $capacidadeMaxima;
     private $itens;
 
-    public function __construct($capacidadeMaxima, $itens)
+    public function __construct($capacidadeMaxima)
     {
         $this->capacidadeMaxima = $capacidadeMaxima;
         $this->itens = array[]; 
@@ -13,6 +13,28 @@ class Inventario
 
     public function getCapacidadeMaxima()
     {
-        
+        return $this->capacidadeMaxima;
+    }
+    
+    public function setCapacidadeMaxima($capacidadeMaxima)
+    {
+        $this->capacidadeMaxima = $capacidadeMaxima;
+    }
+
+    public function adicionarItem($item)
+    {
+        return array_push($this->itens, $item);
+    }
+
+    public function removerItem($item)
+    {
+        return array_search($item, $this->itens);;
+    }
+
+    public function capactidadeLivre()
+    {
+        $valor = $this->capacidadeMaxima - count($this->itens)
+        return $valor;
     }
 }
+
